f8e9e1fdf5dbda161cb28f2f02fab6cf991d415b
Ankesh <ankeshgang@gmail.com>
2014-05-05 15:38:12 +0530

    Create SolrConfig.php

---
+++ Config/SolrConfig.php
@@ -0,0 +1,11 @@
+<?php
+//define solr collection 
+$config['solrCollection'] = 'collection name';
+
+//difine solr credentials for create connection
+$config['solr'] = array(
+    'host' => 'solr host name',
+    'port' => 'solr port',
+    'path' => 'solr/'
+);
+?>
